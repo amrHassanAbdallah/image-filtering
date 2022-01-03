bdd333e0d58ebd39f7b2e802aec44e01209eea6b
Amr Hassan <amrh434@gmail.com>
2022-01-03 03:04:27 +0200

    chore : updated the ports.

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a9aa7905fa2ef4a2c803ba3ce1dc33ef-1862655364.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
